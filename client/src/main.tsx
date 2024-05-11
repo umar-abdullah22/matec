0ab1dbcd6d5894136ad726d3e222b4ab804e801c
umar-abdullah22 <umar.coderzhunt@gmail.com>
2024-05-11 12:34:34 +0500

    added one comment for development mode

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,7 +3,7 @@ import App from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  // <React.StrictMode>
+  // <React.StrictMode> // for development mode
     <App />
   // </React.StrictMode>
 );
